dc7fd82bb79fe894f4222203df2d9f6f961a03fe
Nicholas Pizzigati <npizzigati@gmail.com>
2020-11-24 12:39:24 -0600

    Implement confirmation dialog in Javascript for delete

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,1 +1,10 @@
-console.log('hello world')
+// Delete confirmation
+$('form.delete').submit(function(event) {
+    let deleteMe = confirm('Really delete?');
+    event.preventDefault();
+    event.stopPropagation();
+    if (deleteMe) {
+        console.log(this)
+        this.submit()
+    }
+});
