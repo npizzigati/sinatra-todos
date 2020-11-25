93e291987588bd8f0a73def8ece96007c3585e02
Nicholas Pizzigati <npizzigati@gmail.com>
2020-11-25 11:36:02 -0600

    Change jquery to use minified version

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -4,7 +4,25 @@ $('form.delete').submit(function(event) {
     event.preventDefault();
     event.stopPropagation();
     if (deleteMe) {
-        console.log(this)
-        this.submit()
+        // console.log(this)
+        // this.submit()
+        form = $(this);
+        $.ajax({
+            url: form.attr('action'),
+            method: 'post',
+            dataType: 'text',
+            success: function(todoType) {
+                console.log(todoType);
+                if (todoType == 'item') {
+                    form.parent().remove();
+                } else {
+                    window.location.href = '/lists';
+                } 
+            },
+            error: function(data) {
+                console.log('error!');
+            }
+        });
+        
     }
 });
